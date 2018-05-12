847d3228de889ddf677625570df74aa84bceff06
selmi <selmi.marouen@hotmail.com>
2018-05-12 20:35:42 +0200

    permière version sans css

--- connexion/views.py
+++ connexion/views.py
@@ -7,9 +7,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import *
 from django.views.generic import TemplateView
 from django.conf import settings
+from django.contrib.auth.models import User
+from .forms import UserForm
 
 def home(request):
     return render(request, 'connexion/home.html', {})
+
+
 
 class LoginView(TemplateView):
   template_name = 'connexion/index.html'
@@ -19,9 +23,8 @@ class LoginView(TemplateView):
     user = authenticate(username=username, password=password)
     if user is not None and user.is_active:
         login(request, user)
-        print('Bonjouuuuur')
         return HttpResponseRedirect('home')
-    return render(request, self.template_name , {'user':user})
+        return render(request, self.template_name , {'user':user})
 
 class LogoutView(TemplateView):
   template_name = 'connexion/index.html'
